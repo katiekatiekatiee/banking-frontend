01b76e7123d8aa25f7902ce45ef836f9d3aa7ef3
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-12-01 23:18:38 -0500

    imported provider and router

--- src/index.js
+++ src/index.js
@@ -15,3 +15,22 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './redux/store'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
