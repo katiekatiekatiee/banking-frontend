f355a0b2a79eadac0ac8fe8ae42a356e028d4bf8
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-12-01 23:44:01 -0500

    set up reducer outline with default state

---
+++ src/redux/reducer.js
@@ -0,0 +1,11 @@
+function reducer(state = {goals: [], transactions: []}, action) {
+    switch(action.type) {
+        // case value:
+        // break;
+        default:
+            return state;
+    }
+}
+
+export default reducer;
+
