a9347f1b04f3c0f3f286009ae986cf6b1666d130
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-12-05 13:10:23 -0500

    initiated fetch for new transaction

--- src/redux/transactionsActions.js
+++ src/redux/transactionsActions.js
@@ -7,3 +7,18 @@ export const postTransactions = () => {
       .then(transactions => dispatch({type: "POST_TRANSACTIONS", payload: transactions}))
     }
   }
+
+export const addTransaction = (trans) => {
+    return (dispatch) => {
+      fetch("http://localhost:3000/transactions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(trans)
+      })
+      .then(r => r.json())
+      .then(transaction => dispatch({type: "ADD_TRANSACTION", payload: transaction}))
+    }
+  }
