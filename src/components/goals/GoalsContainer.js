df27134e0a493f43ee92f6d21897e139cff358af
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-12-05 12:40:06 -0500

    mapped dispatch and state to props for goals

--- src/components/goals/GoalsContainer.js
+++ src/components/goals/GoalsContainer.js
@@ -3,13 +3,27 @@ import { connect } from 'react-redux'
 
 import React, { Component } from 'react'
 
-export default class GoalsContainer extends Component {
+class GoalsContainer extends Component {
     render() {
         return (
             <div>
-                
+                We got dem goals
             </div>
         )
     }
 }
 
+function mapStateToProps(state){
+    return{
+        goals: state.goals 
+    }
+
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        dispatchPostGoals: () => dispatch(postGoals())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalsContainer)
