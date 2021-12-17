da3e3aaba82b9365f1b859f2ed5db35cb030e98f
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-12-16 23:32:36 -0500

    refactored transaction display

---
+++ src/components/transactions/TransactionCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { deleteTransaction } from './../../redux/transactionsActions';
+import { connect } from 'react-redux';
+
+const TransactionCard = (props) => {
+    function handleDelete () {
+        // debugger
+        // console.log(this.props.transaction) //transaction is undefined
+        props.dispatchDeleteTransaction(parseInt(props.id))
+      }
+
+      return(
+          <div>
+                <ul key={props.id}>
+                    
+                    <li>Date: {props.date}<br/>
+                        Amount: ${props.amount}<br/>
+                        Goal: {props.goals.find(({id}) => {
+                            return id === props.goal_id}).name}<br/>
+                    <button onClick={() => handleDelete(props.id)}>Delete Transaction</button></li><br/>
+                </ul>
+
+            </div>
+      )
+
+}
+
+const mapStateToProps = state => ({
+    
+    goals: state.goals
+
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    
+            dispatchDeleteTransaction: (id) => dispatch( deleteTransaction(id) )
+        
+    })
+
+export default connect(mapStateToProps, mapDispatchToProps)(TransactionCard)
+
--- src/components/transactions/TransactionsContainer.js
+++ src/components/transactions/TransactionsContainer.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 //import { postTransactions } from './../../redux/transactionsActions'
-import { deleteTransaction } from './../../redux/transactionsActions'
+// import { deleteTransaction } from './../../redux/transactionsActions';
+import TransactionCard from './TransactionCard';
 
 class TransactionsContainer extends Component {
 
@@ -9,50 +10,65 @@ class TransactionsContainer extends Component {
     //     this.props.dispatchPostTransactions()
     // }
 
-    handleDelete = (transId) => {
-        // debugger
-        // console.log(this.props.transaction) //transaction is undefined
-        this.props.dispatchDeleteTransaction(parseInt(transId, 10))
-      }
+    // handleDelete = (transId) => {
+    //     // debugger
+    //     // console.log(this.props.transaction) //transaction is undefined
+    //     this.props.dispatchDeleteTransaction(parseInt(transId, 10))
+    //   }
 
-    render(){
-
+    // handleSearch = (e) => {
+    //     // debugger
+    //     this.props.transactions.filter((trans) => {
+    //         return e.target.value = trans.date
+    //     })
+    // }
+    render() {
         return (
-            <div>
-                <h2>Transaction History</h2>
-                <div>
-                    {this.props.transactions.map((transaction) => {
-
-                    const findGoal = this.props.goals.find(({id}) => {
-                        // console.log(id, transaction.goal_id)
-                        return id === transaction.goal_id})
-                        return <ul key={transaction.id}>
-                        
-                        <li>Date: {transaction.date}<br/>
-                        Amount: ${transaction.amount}<br/>
-                        Goal: {findGoal && findGoal.name}<br/>
-                        <button onClick={() => this.handleDelete(transaction.id)}>Delete Transaction</button></li><br/>
-                    </ul>})}
-    
-                </div>
-            </div>
-        )
+        <div className="transactions-list">
+            <h2>Transaction History</h2>
+            {this.props.transactions.map(t => <TransactionCard key={t.id} {...t} /> )}
+        </div>
+        );
     }
+
+    // render(){
+
+    //     return (
+    //         <div>
+    //             <h2>Transaction History</h2>
+    //             <div>
+    //                 {/* <p>Search Transactions by date (MM/DD/YY): <input type="text" placeholder="Search..." onChange={this.handleSearch}></input></p> */}
+    //                 {this.props.transactions.map((transaction) => {
+
+    //                 const findGoal = this.props.goals.find(({id}) => {
+    //                     // console.log(id, transaction.goal_id)
+    //                     return id === transaction.goal_id})
+    //                     return <ul key={transaction.id}>
+                        
+    //                     <li>Date: {transaction.date}<br/>
+    //                     Amount: ${transaction.amount}<br/>
+    //                     Goal: {findGoal && findGoal.name}<br/>
+    //                     <button onClick={() => this.handleDelete(transaction.id)}>Delete Transaction</button></li><br/>
+    //                 </ul>})}
+    
+    //             </div>
+    //         </div>
+    //     )
+    // }
    
 }
 
-function mapStateToProps(state){
-    return{
+const mapStateToProps = state => ({
+    
         transactions: state.transactions, 
         goals: state.goals
-    }
 
-}
+})
 
-function mapDispatchToProps(dispatch) {
-    return {
-        dispatchDeleteTransaction: (id) => dispatch( deleteTransaction(id) )
-    }
-}
+// const mapDispatchToProps = (dispatch) => ({
+    
+//         dispatchDeleteTransaction: (id) => dispatch( deleteTransaction(id) )
+    
+// })
 
-export default connect(mapStateToProps, mapDispatchToProps)(TransactionsContainer)
+export default connect(mapStateToProps)(TransactionsContainer)
