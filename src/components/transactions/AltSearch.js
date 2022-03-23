2b8e79afe6a4426a4244ea7c65dbb3729f7b55e4
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2022-03-22 21:45:38 -0400

    exported component and outlined handlechange

--- src/components/transactions/AltSearch.js
+++ src/components/transactions/AltSearch.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { searchTransactions } from '../../redux/transactionsActions'
 
-export default class AltSearch extends Component {
+class AltSearch extends Component {
     state = {
         searchTerm: ''
     }
 
+    handleChange = () => {
+
+    }
+
+    handleSubmit = () => {
+        
+    }
+
+
     render() {
         return(
         <div>
-            <h1>HELLO WORLD</h1>
-           
+            
+            <form onSubmit={this.handleSubmit}>
+                <label>Enter Date to Search: </label>
+                <input type="text" placeholder="Search..." name="search" onChange={this.handleChange} value={this.state.searchTerm}/>
+                <input type="submit" value="Search"/>
+            </form>
         </div>
         )}
 }
+
+export default connect(null, {searchTransactions})(AltSearch)
 
