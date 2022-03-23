8f5ed3d3c320dfd87978182e1352ccfd3b679565
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2022-03-22 21:18:03 -0400

    set initial search state

--- src/components/transactions/AltSearch.js
+++ src/components/transactions/AltSearch.js
@@ -1,3 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { searchTransactions } from '../../redux/transactionsActions'
+
+class altSearch extends Component {
+    state = {
+        searchTerm: ''
+    }
+
+    
+}
